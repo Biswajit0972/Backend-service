3221e2a2b545fbe3674f5c666c66f1e92cd02c39
Biswajit0972 <workbiswajit0981@gmail.com>
2024-09-26 10:09:18 +0530

    bugs fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
-import { app } from './app.js';
-import { dbConnection } from './db/databaseConnection.js';
+import { app } from './src/app.js';
+import { dbConnection } from './src/db/databaseConnection.js';
 
 dbConnection().then(app.listen(process.env.PORT, () => {
     console.log(`server is running on http://localhost:${process.env.PORT}/`)
--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ import { userRouter } from "./router/user.router.js";
 import { noteRouter } from "./router/note.router.js";
 
 const app = express();
-
+app.get("/", (req, res) => {
+    res.send("hey ").status(200)
+})
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
