c26929338bfcb27770a3b161db40baee18b84085
Biswajit0972 <workbiswajit0981@gmail.com>
2024-09-26 10:22:00 +0530

    Add a welcome text

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ import { userRouter } from "./router/user.router.js";
 import { noteRouter } from "./router/note.router.js";
 
 const app = express();
-app.get("/", (req, res) => {
-    res.send("hey ").status(200)
+
+app.get("/", (_, res) => {
+    res.status(200).send("welcome to the backend-service")
 })
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
