642de71ea561c4dd50dd2a64e58b70e2ca9ce24b
Biswajit0972 <workbiswajit0981@gmail.com>
2025-02-16 20:38:25 +0530

    For testing purpose

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import 'dotenv/config';
 import { app } from './src/app.js';
 import { dbConnection } from './src/db/databaseConnection.js';
 
-dbConnection().then(app.listen(process.env.PORT, () => {
-    console.log(`server is running on http://localhost:${process.env.PORT}/`)
-}))
+// dbConnection().then(app.listen(process.env.PORT, () => {
+//     console.log(`server is running on http://localhost:${process.env.PORT}/`)
+// }))
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+})
