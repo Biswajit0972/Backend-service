473b960c995e900923937360fcde3912afa1d837
Biswajit0972 <workbiswajit0981@gmail.com>
2025-02-16 20:49:33 +0530

    fix error handling.

--- src/utils/AsyncHandler.js
+++ src/utils/AsyncHandler.js
@@ -1,11 +1,10 @@
+import {ApiError} from "./ApiError.js";
+
 const asyncHandler = (fn) => async (req, res, next) => {
   try {
     await fn(req, res, next);
   } catch (err) {
-    res.status(err.code || 500).json({
-      success: false,
-      message: err.message,
-    });
+    res.status(err.code || 500).json(new ApiError(err.statusCode || 500, err.message || "Something went wrong",));
   }
 };
 
