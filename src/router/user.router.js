fff240ad7172b993e7337b0a1289924566071eb0
Biswajit0972 <workbiswajit0981@gmail.com>
2025-02-17 16:13:47 +0530

    update user router

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -2,13 +2,13 @@ import express from "express";
 import { verifyAuth } from "../middleware/verifyAuth.js";
 import { registerUser, loginUser, logOut, updateUser, changeUserPassword, getUserData } from "../controllers/User.controller.js";
 const userRouter = express.Router();
-userRouter.route("/app/v1/signup").post(registerUser)
-userRouter.route("/app/v1/signin").post(loginUser)
+userRouter.route("/v1/signup").post(registerUser)
+userRouter.route("/v1/signin").post(loginUser)
 
 // Auth routes
-userRouter.route("/app/v1/logout").get(verifyAuth, logOut)
-userRouter.route("/app/v1/updateprofile").post(verifyAuth, updateUser);
-userRouter.route("/app/v1/changepassword").post(verifyAuth, changeUserPassword);
-userRouter.route("/app/v1/getuser").get(verifyAuth, getUserData);
+userRouter.route("/v1/logout").get(verifyAuth, logOut)
+userRouter.route("/v1/updateprofile").post(verifyAuth, updateUser);
+userRouter.route("/v1/changepassword").post(verifyAuth, changeUserPassword);
+userRouter.route("/v1/getuser").get(verifyAuth, getUserData);
 
 export {userRouter}
